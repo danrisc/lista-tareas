af5c0d6efe9a52c91e61318a71b391f33821f41f
daniela risco <driscop@gmail.com>
2017-04-27 12:27:09 -0300

    tratando de esclarecer

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 var res= [Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object…];
+var x = [Object, Object, Object, Object, Object, Object, Object, Object, Object, Object] 
+var  = [];
 
+var input= document.getElementById("Añadir Tarea");
+element.forEach(
+    function (objet){
+    
